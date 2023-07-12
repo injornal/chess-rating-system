2ef85a1549b6ca183c4f9690db07f5d13f06b0c9
Kostia <babich.kostia@gmail.com>
2023-07-11 18:48:40 -0700

    test removed

--- src/chrate/blueprints/home/home.py
+++ src/chrate/blueprints/home/home.py
@@ -6,8 +6,3 @@ home_bp = blueprints.Blueprint("home", __name__, template_folder="templates")
 @home_bp.route("/")
 def home():
     return render_template("home.html")
-
-
-@home_bp.route("/1234")
-def foo():
-    return "1234"
