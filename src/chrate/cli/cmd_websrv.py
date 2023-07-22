e020590d468f54650f4d40590e8c602984d6e3f2
Kostia <babich.kostia@gmail.com>
2023-07-21 18:31:28 -0700

    some frontend improvements

--- src/chrate/cli/cmd_websrv.py
+++ src/chrate/cli/cmd_websrv.py
@@ -6,5 +6,4 @@ from chrate.app import app
 @click.group(invoke_without_command=True, help="Run webserver")
 @click.option("-d", "--debug", is_flag=True, help="Run in debug mode")
 def websrv(debug):
-    print("bla")
     app.run(debug=debug)
